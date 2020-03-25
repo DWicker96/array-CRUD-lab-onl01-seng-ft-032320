d4d8cb2dfb98cd8e983421e9de91cdb0c1a99efb
Dakota Wicker <dwicker1996@gmail.com>
2020-03-25 17:14:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-<< "rob zombie"
+add_element_to_end_of_array << "rob zombie"
 end
 
 def add_element_to_start_of_array(array, element)
